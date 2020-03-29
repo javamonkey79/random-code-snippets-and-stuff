239a059b96b3ffd2792102aa31615fdada0c5ec2
Shaun Elliott <javamonkey79@gmail.com>
2020-03-29 11:11:48 -0700

    Update resume-paragrapher-breaker.py

--- resume-paragrapher-breaker.py
+++ resume-paragrapher-breaker.py
@@ -1,18 +1,22 @@
-import re
-input = """
-My objective is to enable pragmatic and efficient software
-designs, architectures and implementation solutions whatever the
-environment or application domain. I achieve this with 
-dedication, problem solving, outstanding personal skills and a 
-commitment to engage an ever changing and dynamic technical 
-field.
-"""
-line = ""
+import re, sys
 
+max_line_len = 65
+if len(sys.argv) > 1:
+    # assuming the user is smart enough to pass an int...
+    max_line_len = int(sys.argv[1])
+
+input = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
+if len(sys.argv) > 2:
+    input = sys.argv[2]
+
+line = ""
 words = re.split("\\s+", input)
 for w in words:
     if len(w) > 0:
-        if len(line) + len(w) < 65:
+        # 1 accounting for space
+        if len(line) + len(w) + 1 < max_line_len:
             if len(line) > 0:
                 line = line + " " + w
             else:
