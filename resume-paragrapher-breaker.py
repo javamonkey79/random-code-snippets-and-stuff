2f9e60ae3638dcddcc45030f9fb32463f79431fe
Shaun Elliott <javamonkey79@gmail.com>
2020-03-29 11:22:54 -0700

    Update resume-paragrapher-breaker.py

--- resume-paragrapher-breaker.py
+++ resume-paragrapher-breaker.py
@@ -5,11 +5,7 @@ if len(sys.argv) > 1:
     # assuming the user is smart enough to pass an int...
     max_line_len = int(sys.argv[1])
 
-input = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-"""
-if len(sys.argv) > 2:
-    input = sys.argv[2]
+input = input("--> ")
 
 line = ""
 words = re.split("\\s+", input)
